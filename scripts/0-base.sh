ecd23cae92d7f3cb03f00246054cf628a10065be
hhhhhhhhhn <joseugarte2262005@gmail.com>
2021-08-24 00:48:49 -0400

    initial commit

---
+++ scripts/0-base.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#: System Clock
+timedatectl set-ntp true
+
+#: Partitions
+wipefs -a "/dev/$DRIVE"
+
+gdisk "/dev/$DRIVE" < assets/gdisk.txt
+
+mkfs.fat -F32 "/dev/${DRIVE}2"
+mkfs.ext4 "/dev/${DRIVE}3"
+
+mount "/dev/${DRIVE}3" /mnt
+mkdir -p /mnt/boot/EFI
+mount "/dev/${DRIVE}2" /mnt/boot/EFI
+
+#: Base system
+pacstrap /mnt base linux linux-firmware grub networkmanager sudo lxde
+
+genfstab -U /mnt >> /mnt/etc/fstab
---
+++ scripts/1-basechroot.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#: Configuration
+ln -sf "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
+hwclock --systohc
+
+mv /etc/locale.gen /etc/locale.gen.old
+echo "$LOCALES" | tr "|" "\n" > /etc/locale.gen
+locale-gen
+echo "LANG=$(tail -n 1 < /etc/locale.gen | sed "s/\s*$//")" > /etc/locale.conf
+echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
+echo Arch > /etc/hostname
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+systemctl enable NetworkManager
+systemctl enable lxdm
+
+yes "$ROOTPASSWORD" | passwd
+useradd -m -G wheel "$USERNAME"
+yes "$USERPASSWORD" | passwd "$USERNAME"
+
+#: Bootloader
+grub-install "--target=$(uname -m | grep 64 >/dev/null && echo x86_64 || echo i386)-efi" --recheck --removable --efi-directory=/boot/EFI --boot-directory=/boot
+
+grub-install --target=i386-pc --recheck --boot-directory=/boot "/dev/$DRIVE"
+
+grub-install --target=i386-pc --recheck --boot-directory=/boot "/dev/${DRIVE}3"
+
+grub-mkconfig -o /boot/grub/grub.cfg
